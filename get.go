1d9beff0da53316d415cb10fdedc30093d93405a
Joel Scoble <joel.scoble@outlook.com>
2014-09-11 14:21:32 -0500

    added non-error returning versions of some gets

--- get.go
+++ get.go
@@ -1,47 +1,35 @@
 package contour
 
-// Get returns the setting Value as an interface{}.
-func Get(k string) (interface{}, error) {
-	_, ok := AppConfig.settings[k]
-	if !ok {
-		return nil, notFoundErr(k)
-	}
 
-	return AppConfig.settings[k].Value, nil
+// E versions, these return the error. Non-e versions are just wrapped calls to
+// these functions with the error dropped.
+
+// Get returns the setting Value as an interface{}.
+func Get(k string) interface{} {
+	tmp, _ := GetE(k)
+	return tmp
 }
 
 // GetBool returns the setting Value as a bool.
-func GetBool(k string) (bool, error) {
-	_, ok := AppConfig.settings[k]
-	if !ok {
-		return false, notFoundErr(k)
-	}
-
-	return *AppConfig.settings[k].Value.(*bool), nil
+func GetBool(k string) bool {
+	tmp, _ := GetBoolE(k)
+	return tmp
 }
 
 // GetInt returns the setting Value as an int.
-func GetInt(k string) (int, error) {
-	_, ok := AppConfig.settings[k]
-	if !ok {
-		return 0, notFoundErr(k)
-	}
-
-	return *AppConfig.settings[k].Value.(*int), nil
+func GetInt(k string) int {
+	tmp, _ := GetIntE(k)
+	return tmp
 }
 
 // GetString returns the setting Value as a string.
-func GetString(k string) (string, error) {
-	_, ok := AppConfig.settings[k]
-	if !ok {
-		return "", notFoundErr(k)
-	}
-
-	return *AppConfig.settings[k].Value.(*string), nil
+func GetString(k string) string {
+	tmp, _ := GetStringE(k)
+	return tmp
 }
 
 // GetInterface is a convenience wrapper function to Get
-func GetInterface(k string) (interface{}, error) {
+func GetInterface(k string) interface{} {	
 	return Get(k)
 }
 
@@ -82,4 +70,49 @@ func GetStringFilterNames() []string {
 	}
 
 	return names
+}
+
+// GetE returns the setting Value as an interface{}.
+func GetE(k string) (interface{}, error) {
+	_, ok := AppConfig.settings[k]
+	if !ok {
+		return nil, notFoundErr(k)
+	}
+
+	return AppConfig.settings[k].Value, nil
+}
+
+// GetBoolE returns the setting Value as a bool.
+func GetBoolE(k string) (bool, error) {
+	_, ok := AppConfig.settings[k]
+	if !ok {
+		return false, notFoundErr(k)
+	}
+
+	return *AppConfig.settings[k].Value.(*bool), nil
+}
+
+// GetIntE returns the setting Value as an int.
+func GetIntE(k string) (int, error) {
+	_, ok := AppConfig.settings[k]
+	if !ok {
+		return 0, notFoundErr(k)
+	}
+
+	return *AppConfig.settings[k].Value.(*int), nil
+}
+
+// GetStringE returns the setting Value as a string.
+func GetStringE(k string) (string, error) {
+	_, ok := AppConfig.settings[k]
+	if !ok {
+		return "", notFoundErr(k)
+	}
+
+	return AppConfig.settings[k].Value.(string), nil
+}
+
+// GetInterfaceE is a convenience wrapper function to Get
+func GetInterfaceE(k string) (interface{}, error) {
+	return GetE(k)
 }
